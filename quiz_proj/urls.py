2092eb9be82372cbc9b290f4ff0e3c3e5c20e138
Rohan Raj <rohan@gmail.com>
2021-11-27 22:43:36 +0530

    second

--- quiz_proj/urls.py
+++ quiz_proj/urls.py
@@ -6,7 +6,7 @@ Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
+     Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
@@ -17,6 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+# Django admin customization
+admin.site.site_header = "Online Quiz"
+admin.site.site_title = "Welcome to our Website"
+admin.site.index_title ="Welcome to this portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
